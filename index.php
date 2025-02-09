ad038f40d57bfccfd1a3aa21ccca412d0c21a755
is4ac-romero <is4ac.rom3ero@hotmail.com>
2025-02-08 21:39:27 -0300

    init login development

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Page</title>
+    <script src="./assets/script/select-typeof-user.js" defer></script>
 </head>
 <body>
-    
+    <form action="">
+        <select name="user-type" id="user-type">
+            <option value="admin">Administrador</option>
+            <option value="aluno">Aluno</option>
+        </select>
+    </form>
 </body>
 </html>
