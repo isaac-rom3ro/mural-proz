47a2493218a67e228fcd60ccfa0bd74532b4b73e
isaac-rom3ro <is4ac.romero@outlook>
2025-03-28 21:50:25 -0300

    starting again

--- assets/methods/content-filter.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $filter = $_POST["filter"]; //get the filter
-        
-        //change the url 
-        header("Location: ../../pages/mural.php?filter=$filter");   
-    }
